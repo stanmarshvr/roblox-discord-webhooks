e42d017acf335af4c658d9526d263f21744aac12
stan <30905705+stanmarshvr@users.noreply.github.com>
2024-12-29 17:57:06 -0800

    Update DiscordWebhooks.lua

--- DiscordWebhooks.lua
+++ DiscordWebhooks.lua
@@ -1,5 +1,7 @@
 local module = {}
-module.__index = module
+module.Webhook = {}
+module.Utils = {}
+module.Webhook.__index = module.Webhook
 local config = require(script.Config)
 local HttpService = game:GetService("HttpService")
 
@@ -9,13 +11,13 @@ local function printIfDebug(thing)
 	end
 end
 
-function module.new(webhookUrl)
+function module.Webhook.new(webhookUrl: string)
 	local response = HttpService:RequestAsync({Url = webhookUrl, Method = "GET"})
 	printIfDebug("Webhook Get Response: "..response.Body)
 	local decodedResponse = HttpService:JSONDecode(response.Body)
 	if response.Success then
 		printIfDebug("Webhook valid")
-		local self = setmetatable({}, module)
+		local self = setmetatable({}, module.Webhook)
 		self.initialized = true
 		self.webhookName = decodedResponse["name"]
 		self.url = webhookUrl
@@ -26,13 +28,20 @@ function module.new(webhookUrl)
 	end
 end
 
-function module.rgbToColor(red, green, blue)
+function module.Utils.rgbToColor(red: number, green: number, blue: number) : number
 	return bit32.lshift(red, 16) + bit32.lshift(green, 8) + blue
 end
 
-print(module.rgbToColor(255, 20, 20))
+function module.Utils.CheckWebhook(url: string) : boolean
+	local response = HttpService:RequestAsync({Url = url, Method = "GET"})
+	return response.Success
+end
 
-function module:SendMessage(message: string, username: string)
+function module.Webhook:Valid()
+	return module.Utils.CheckWebhook(self.url)
+end
+
+function module.Webhook:SendMessage(message: string, username: string)
 	if not self.initialized then return end
 	assert(#message <= 2000, "Message too long!")
 	self.webhookName = username or self.webhookName
@@ -43,12 +52,17 @@ function module:SendMessage(message: string, username: string)
 	local response = HttpService:PostAsync(self.url, HttpService:JSONEncode(requestTable), Enum.HttpContentType.ApplicationJson)
 end
 
-function module:SendEmbed(title: string, description: string, url: string, color: number, fields: {})
+function module.Webhook:Delete()
+	if not self.initialized then return end
+	local response = HttpService:RequestAsync({Url = self.url, Method = "DELETE"})
+end
+
+function module.Webhook:SendEmbed(title: string, description: string, url: string, color: number, fields: {})
 	if not self.initialized then return end
 	local embedTable = {["embeds"] = {{
 		["title"] = title or "Roblox",
-		["description"] = description,
-		["url"] = url or "https://www.roblox.com/games/79387422884028",
+		["description"] = description or "From Roblox",
+		["url"] = url or "https://www.roblox.com",
 		["color"] = color or 16716820,
 		["fields"] = fields
 		}}
@@ -61,7 +75,6 @@ function module:SendEmbed(title: string, description: string, url: string, color
 	else
 		printIfDebug("Unsuccessful embed response: ")
 		printIfDebug(response)
-		error("Embed failed to send to Discord!")
 	end
 end
 
